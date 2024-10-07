616816aeb8caa272f4ae2091e91d2900c4648409
ARSHamirali <arshamirali604@gmail.com>
2024-10-07 12:58:53 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,54 @@
+// Home Work
+
+// Date & Time
+
+// Q1
+// var date = Date()
+// console.log(date)
+
+// Q2
+// var mouth=["Jan","Feb","March","April","May","Jun","Jule","August","September","October","Decmeber"]
+// for(var i =0 ; i<mouth.length ; i++){
+//     console.log(mouth[i])
+// }
+
+
+//Q3
+// var date = new Date()
+//   console.log(date.toString().slice(0,3))
+ 
+
+
+// var userday=prompt('Enter the day')
+// var day=["SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"]
+// if( "SUNDAY" || "SATURDAY" === userday){
+// console.log("Funday")
+// }
+// else{
+//     console.log("Week day")
+// }
+
+
+
+
+
+
+
+
+
+// Q6
+// var date= Date()
+// console.log(date)
+// var milisecond1970=date.getTime()
+// console.log(milisecond1970)
+// var min1970=milisecond1970*(60*60)
+// console.log(min1970);
+
+
+
+// var current = Date().getHours()
+// console.log(current);
+
+
+// var current=new Date().getTime()
+// console.log(current)
